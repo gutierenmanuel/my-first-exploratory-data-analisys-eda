4f93ccf94014e6e9831b71e6e7b4f3c05cbced45
gutierenmanuel <gutierenmanuel15@gmail.com>
2023-11-22 05:12:09 +0100

    saved data, eda done and functions updated

--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,37 @@
-from dotenv import load_dotenv
-from sqlalchemy import create_engine
-import pandas as pd
 
-# load the .env file variables
-load_dotenv()
+## Crea un grafico con subgraficos para cada histograma con el limite de columnas indicado 
 
+import seaborn as sns
+import matplotlib.pyplot as plt
+import math
 
-def db_connect():
-    import os
-    engine = create_engine(os.getenv('DATABASE_URL'))
-    engine.connect()
-    return engine
+def plot_histograms(dataframe, max_columns=3):
+    # Obtén la lista de columnas categóricas
+    categorical_columns = dataframe.select_dtypes(include=['object']).columns.tolist()
+
+    # Calcula el número total de variables categóricas y el número de filas y columnas deseadas
+    num_categorical = len(categorical_columns)
+    num_cols = min(num_categorical, max_columns)
+    num_rows = math.ceil(num_categorical / num_cols)
+
+    # Configura el tamaño del gráfico
+    plt.figure(figsize=(5 * max_columns, 5 * num_rows))
+
+    # Itera sobre las columnas categóricas y crea un histograma para cada una
+    for i, column in enumerate(categorical_columns, start=1):
+        plt.subplot(num_rows, num_cols, i)
+        sns.histplot(data=dataframe, x=column, kde=True)  # Puedes ajustar las opciones según tu preferencia
+        plt.title(f'Histograma de {column}')
+        plt.xlabel(column)
+        plt.ylabel('Frecuencia')
+
+    # Ajusta el espaciado entre subgráficos
+    plt.tight_layout()
+
+    # Muestra el gráfico
+    plt.show()
+
+# Ejemplo de uso
+# Supongamos que tienes un DataFrame llamado 'df' con las variables categóricas
+# Puedes llamar a la función de la siguiente manera:
+# plot_histograms(df, max_columns=4)
